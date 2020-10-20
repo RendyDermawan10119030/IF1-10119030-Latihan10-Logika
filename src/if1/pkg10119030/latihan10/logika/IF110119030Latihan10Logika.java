0959254d7b5be1bc3e716c98ae9f85aa6a8a0609
RendyDermawan10119030 <rendy.10119030@mahasiswa.unikom.ac.id>
2020-10-20 22:49:59 +0700

    First commit

---
+++ src/if1/pkg10119030/latihan10/logika/IF110119030Latihan10Logika.java
@@ -0,0 +1,17 @@
+package if1.pkg10119030.latihan10.logika;
+/**
+ * Nama     : Rendy Dermawan
+ * Kelas    : IF-1
+ * NIM      : 10119030
+ * Deskripsi Program :Latihan Teks
+ */
+    public class IF110119030Latihan10Logika {
+      
+        public static void main(String[] args) {
+            int umur = 19;
+            boolean dewasa;
+            dewasa = (umur >=17);
+            System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
